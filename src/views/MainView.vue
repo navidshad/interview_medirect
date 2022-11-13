481be16d2373ecfa3401b9e6cb0dba58147bb66d
Navid Shad <navidshad72@gmail.com>
2022-11-13 21:04:39 +0300

    [ tweak ]

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>Main Page</div>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="max-w-2xl">
+      <h1>Forex Exchange</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style>
-
 </style>
